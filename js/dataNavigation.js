bce54a6a67e3aa95c58ffa172c97faa6aa8ddd7f
jyh7a <40563068+jyh7a@users.noreply.github.com>
2019-07-25 18:23:22 +0900

    Update dataNavigation.js
    dataInit(); // 데이터 초기화후 그려줌
    위치수정

--- js/dataNavigation.js
+++ js/dataNavigation.js
@@ -7,19 +7,19 @@ $('.button').on('click', function (e) {
 
 	if (e.target.textContent == 'next') {
 		if (dataList[dataIndex + 1]) {
-			dataIndex++;
-			dataInit(); // 데이터 초기화후 그려줌
+			dataIndex++;			
 		} else {
 			alert('마지막 문제입니다 ㅠㅅㅠ;;');
 		}
 	} else if (e.target.textContent == 'prev') {
 		if (dataList[dataIndex - 1]) {
-			dataIndex--;
-			dataInit(); // 데이터 초기화후 그려줌
+			dataIndex--;		
 		} else {
 			alert('맨 앞입니다...;;');
 		}
 	}
+
+	dataInit(); // 데이터 초기화후 그려줌
 
 	// 턴초기화
 	turn = 'black';
